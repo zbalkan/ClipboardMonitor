2fbccacd99d21d968535e76fdeb56d9214aa23a6
Zafer Balkan <zafer@zaferbalkan.com>
2023-08-01 11:23:52 +0300

    Added default ctor

--- ClipboardMonitor/ProcessInformation.cs
+++ ClipboardMonitor/ProcessInformation.cs
@@ -10,13 +10,20 @@ namespace ClipboardMonitor
 
         public string ExecutablePath { get; set; }
 
+        public ProcessInformation()
+        {
+            WindowTitle = string.Empty;
+            ProcessName = string.Empty;
+            ExecutablePath = string.Empty;
+        }
+
         public readonly override int GetHashCode() => ProcessName.GetHashCode(StringComparison.OrdinalIgnoreCase) ^ ExecutablePath.GetHashCode(StringComparison.OrdinalIgnoreCase);
 
         public static bool operator ==(ProcessInformation left, ProcessInformation right) => left.Equals(right);
 
         public static bool operator !=(ProcessInformation left, ProcessInformation right) => !(left == right);
 
-        public readonly bool Equals(ProcessInformation other) => other != default && other.ProcessName == ProcessName && other.ExecutablePath == ExecutablePath;
+        public readonly bool Equals(ProcessInformation other) => other != null && other.ProcessName == ProcessName && other.ExecutablePath == ExecutablePath;
 
 #pragma warning disable CS8765 // Nullability of type of parameter doesn't match overridden member (possibly because of nullability attributes).
         public readonly override bool Equals(object obj) => obj is ProcessInformation information && Equals(information);

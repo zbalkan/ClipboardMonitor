03a0aeda3bfa7e0b6e1b115970d65c34278fbe5b
Zafer Balkan <zafer@zaferbalkan.com>
2025-09-23 11:27:35 +0300

    Updated test project and CI action

--- ClipboardMonitor.Tests/AmsiTests.cs
+++ ClipboardMonitor.Tests/AmsiTests.cs
@@ -1,17 +1,26 @@
 using ClipboardMonitor.AMSI;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace ClipboardMonitor.Tests
 {
     [TestClass]
     public class AmsiTests
     {
-        private AmsiContext _context = null!;
-        private AmsiSession _session = null!;
+        private AmsiContext _context;
+        private AmsiSession _session;
+        private bool _skip;
 
         [TestInitialize]
         public void Init()
         {
+            // Skip tests in CI environments (GitHub Actions sets CI=true)
+            if (Environment.GetEnvironmentVariable("CI") == "true")
+            {
+                _skip = true;
+                return;
+            }
+
             _context = AmsiContext.Create("UnitTests");
             _session = _context.CreateSession();
         }
@@ -19,6 +28,11 @@ namespace ClipboardMonitor.Tests
         [TestCleanup]
         public void Cleanup()
         {
+            if (_skip)
+            {
+                return;
+            }
+
             _session.Dispose();
             _context.Dispose();
         }
@@ -26,12 +40,24 @@ namespace ClipboardMonitor.Tests
         [TestMethod]
         public void Test_AMSI_Detect_EICAR()
         {
-            Assert.IsTrue(_session.IsMalware(@"X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*", "EICAR"));
+            if (_skip)
+            {
+                Assert.Inconclusive("Skipping AMSI test on CI (Defender not available)");
+            }
+
+            Assert.IsTrue(_session.IsMalware(
+                @"X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*",
+                "EICAR"));
         }
 
         [TestMethod]
         public void Test_AMSI_Skip_False_Positive_EICAR()
         {
+            if (_skip)
+            {
+                Assert.Inconclusive("Skipping AMSI test on CI (Defender not available)");
+            }
+
             Assert.IsFalse(_session.IsMalware("0000", "EICAR"));
         }
     }
---
+++ ClipboardMonitor.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ClipboardMonitor.Tests2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ClipboardMonitor.Tests2")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c65e9898-a115-4310-9979-23a18a849721")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

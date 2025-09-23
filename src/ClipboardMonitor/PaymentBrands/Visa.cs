44fc27b9688eca2f4c625ca21f5d646cf125205e
Zafer Balkan <zafer@zaferbalkan.com>
2025-09-23 15:28:54 +0300

    Simplified Visa regex

--- src/ClipboardMonitor/PaymentBrands/Visa.cs
+++ src/ClipboardMonitor/PaymentBrands/Visa.cs
@@ -5,7 +5,7 @@ namespace ClipboardMonitor.PaymentBrands
     public class Visa : PaymentBrandBase, IPaymentBrand
     {
         private const string _brand = "Visa";
-        private readonly Regex _pattern = new Regex(@"(?:\D|^)(4[0-9]{3}(?:\ |\-|)[0-9]{4}(?:\ |\-|)[0-9]{4}(?:\ |\-|)[0-9]{4})(?:\D|$)", RegexOptions.Compiled);
+        private readonly Regex _pattern = new Regex(@"(?:\D|^)((?:4[0-9]{3})(?:[ \-]?)[0-9]{4}(?:[ \-]?)[0-9]{4}(?:[ \-]?)[0-9]{1,4})(?:\D|$)", RegexOptions.Compiled);
 
         public override Regex GetPattern() => _pattern;
 

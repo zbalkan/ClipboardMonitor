9d7d241b82f631fda7b71ecf10cd6ac8d9609dd5
Zafer Balkan <zafer@zaferbalkan.com>
2025-09-23 22:34:20 +0300

    Added Maestro support

---
+++ src/ClipboardMonitor/PaymentBrands/Maestro.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ClipboardMonitor.PaymentBrands
+{
+    public class Maestro : PaymentBrandBase, IPaymentBrand
+    {
+        private const string _brand = "Maestro";
+
+        private readonly Regex _pattern = new Regex(
+            @"(?:\D|^)((?:5018|5020|5038|5893|6304|6759|6761|6762|6763)(?:[ \-]?[0-9]){8,15})(?:\D|$)",
+            RegexOptions.Compiled);
+
+        public override Regex GetPattern() => _pattern;
+
+        public override string ToString() => _brand;
+    }
+}

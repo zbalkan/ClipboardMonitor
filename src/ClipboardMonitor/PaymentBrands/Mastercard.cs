915c54328e1659b6b9a6049349791378c235fb03
Zafer Balkan <zafer@zaferbalkan.com>
2025-09-23 15:22:10 +0300

    Added 2-Series BIN check for MasterCard

--- src/ClipboardMonitor/PaymentBrands/Mastercard.cs
+++ src/ClipboardMonitor/PaymentBrands/Mastercard.cs
@@ -5,7 +5,7 @@ namespace ClipboardMonitor.PaymentBrands
     public class Mastercard : PaymentBrandBase, IPaymentBrand
     {
         private const string _brand = "Mastercard";
-        private readonly Regex _pattern = new Regex(@"(?:\D|^)(5[1-5][0-9]{2}(?:\ |\-|)[0-9]{4}(?:\ |\-|)[0-9]{4}(?:\ |\-|)[0-9]{4})(?:\D|$)", RegexOptions.Compiled);
+        private readonly Regex _pattern = new Regex(@"(?:\D|^)((?:5[1-5][0-9]{2}|2(?:2[2-9][0-9]|[3-6][0-9]{2}|7[01][0-9]|720))(?:[ \-]?)[0-9]{4}(?:[ \-]?)[0-9]{4}(?:[ \-]?)[0-9]{4})(?:\D|$)", RegexOptions.Compiled);
 
         public override Regex GetPattern() => _pattern;
 
